8158d5c52d6942dfa522e2c91b5deafd542eb140
Steven Ellis <steven.ellis.d@gmail.com>
2021-05-29 09:21:10 -0400

    added signup servlet

---
+++ src/main/java/com/revature/banking/servlets/Signup.java
@@ -0,0 +1,31 @@
+package com.revature.banking.servlets;
+
+import java.io.IOException;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.*;
+import org.hibernate.*;
+
+import com.revature.banking.models.Client;
+import com.revature.banking.services.SessionManager;
+
+@WebServlet("/signup")
+public class Signup extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	public void doPost(HttpServletRequest request, HttpServletResponse response) {
+		String username = request.getParameter("username-input");
+		String password = request.getParameter("password-input");
+		Session session = SessionManager.getSessionFactory().openSession();
+		Transaction tx = session.beginTransaction();
+		session.save(new Client(username, password));
+		tx.commit();
+		session.close();
+		try {
+			response.getWriter().println("Successful account creation");
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+}
