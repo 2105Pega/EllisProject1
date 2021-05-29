69526ae182175027808f802d88abf8590a944d45
Steven Ellis <steven.ellis.d@gmail.com>
2021-05-29 16:43:02 -0400

    sessiomanager returns session instead of factory

--- src/main/java/com/revature/banking/services/SessionManager.java
+++ src/main/java/com/revature/banking/services/SessionManager.java
@@ -1,5 +1,6 @@
 package com.revature.banking.services;
 
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.boot.MetadataSources;
 import org.hibernate.boot.registry.StandardServiceRegistry;
@@ -8,17 +9,19 @@ import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 public class SessionManager {
 	private static SessionFactory sessionFactory = null;
 
-	public static SessionFactory getSessionFactory() {
+	public static Session getSession() {
 		if (sessionFactory == null) {
-			final StandardServiceRegistry registry = new StandardServiceRegistryBuilder().configure().build();
+			StandardServiceRegistry registry = new StandardServiceRegistryBuilder().configure().build();
 			try {
 				sessionFactory = new MetadataSources(registry).buildMetadata().buildSessionFactory();
-				return sessionFactory;
+				return sessionFactory.openSession();
 			} catch (Exception e) {
+				e.printStackTrace();
 				StandardServiceRegistryBuilder.destroy(registry);
+				return null;
 			}
+		} else {
+			return sessionFactory.openSession();
 		}
-		return sessionFactory;
-
 	}
 }
